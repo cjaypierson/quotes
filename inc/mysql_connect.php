f5a23db933ba48a65830c5e657cc9152b3f9186a
Cameron Pierson <cjaypierson@gmail.com>
2013-05-08 18:50:09 -0400

    changed error

--- inc/mysql_connect.php
+++ inc/mysql_connect.php
@@ -1,7 +1,7 @@
 <?php
 $dbc = mysql_connect('localhost', 'pierson3_cameron', 'Summer9(');
 	if (!$dbc) {
-		print('something went wrong');
+		print('********** something went wrong **********');
 	}
 mysql_select_db('myquotes', $dbc);
 ?>
