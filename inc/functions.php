98f7ab6055711fad0110bbf71cc0cb0d45ccbdfc
Cameron Pierson <cjaypierson@gmail.com>
2013-05-09 00:19:42 -0400

    added else clause to fix loop

--- inc/functions.php
+++ inc/functions.php
@@ -3,9 +3,11 @@
 function is_administrator($name = 'Samuel', $value = 'Clemens') {
     if (!isset($_COOKIE[$name]) OR ($_COOKIE[$name] != $value)) {
         print '<h2>Access Denied!</h2>
-        <p class="error">You have to <a href="login.php">login</a> to view this page.</p>';
+        	<p class="error">You have to <a href="login.php">login</a> to view this page.</p>';
         include('tem/footer.php');
         exit();
+    } else {
+    	
     }
 }
 
