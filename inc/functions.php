a53630164e6e8c803da8664d33d1655b8f207c35
Cameron Pierson <cjaypierson@gmail.com>
2013-05-09 00:17:09 -0400

    reversed logic in is_administrator function

--- inc/functions.php
+++ inc/functions.php
@@ -1,12 +1,12 @@
 <?php
 
 function is_administrator($name = 'Samuel', $value = 'Clemens') {
-	if (isset($_COOKIE[$name]) && ($_COOKIE[$name] == $value)) {
-		print '<h2>Access Denied!</h2>
-		<p class="error">You have to <a href="login.php">login</a> to view this page.</p>';
-		include('tem/footer.php');
-		exit();
-	}
+    if (!isset($_COOKIE[$name]) OR ($_COOKIE[$name] != $value)) {
+        print '<h2>Access Denied!</h2>
+        <p class="error">You have to <a href="login.php">login</a> to view this page.</p>';
+        include('tem/footer.php');
+        exit();
+    }
 }
 
 ?>
