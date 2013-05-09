ff065f22498cab1a830c615fe52e7fb1100f1650
Cameron Pierson <cjaypierson@gmail.com>
2013-05-08 20:53:59 -0400

    updated DB name

--- inc/mysql_connect.php
+++ inc/mysql_connect.php
@@ -3,5 +3,5 @@ $dbc = mysql_connect('localhost', 'pierson3_cameron', 'Summer9(');
 	if (!$dbc) {
 		print('********** something went wrong **********');
 	}
-mysql_select_db('myquotes', $dbc);
+mysql_select_db('pierson3_myquotes', $dbc);
 ?>
