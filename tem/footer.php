1aaa50af5710c7d7322e2eec412595ffbafd0124
Cameron Pierson <cjaypierson@gmail.com>
2013-05-09 02:45:52 -0400

    updated footer

--- tem/footer.php
+++ tem/footer.php
@@ -3,6 +3,7 @@
 	if ( (is_administrator() && (basename($_SERVER['PHP_SELF']) != 'logout.php')) OR (isset($loggedin) && $loggedin) ) {
 		print '<hr /><h3>Site Admin</h3>
 				<p><a href="add_quote.php">Add Quote</a> <->
+				<a href="view_quotes.php">View Quotes</a> <->
 				<a href="logout.php">Logout</a></p>';
 	}
 ?>
